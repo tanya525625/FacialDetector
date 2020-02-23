f58f3876d2e9195a1a38c8874446d2d805347101
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2020-02-23 20:53:23 +0300

    Added Face detector using OpenCV Template Matching

--- faces_detection.py
+++ faces_detection.py
@@ -17,30 +17,24 @@ class FaceDetector:
             curr_method = eval(method)
             res = cv2.matchTemplate(img, self.template, curr_method)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-            # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-            if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-                top_left = min_loc
-            else:
-                top_left = max_loc
+            top_left = max_loc
             bottom_right = (top_left[0] + self.template_width, top_left[1] + self.template_height)
 
             cv2.rectangle(img, top_left, bottom_right, 255, 2)
-            _make_plot(img, curr_method)
+            _make_plot(img, method.split('.')[1])
 
 
 def _make_plot(image, method_name):
     plt.imshow(image, cmap='gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(method_name)
+    plt.title(method_name), plt.xticks([]), plt.yticks([])
     plt.show()
 
 
 if __name__ == "__main__":
-    template_path = os.path.join('templates', 'template.jpg')
-    image_path = os.path.join('target_images', 'image_2.jpg')
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-               'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+    template_path = os.path.join('templates', 'template_4.jpg')
+    image_path = os.path.join('target_images', 'image_6.jpg')
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED',
+               'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED']
 
     detector = FaceDetector(template_path, image_path, methods)
     detector.find_faces()
